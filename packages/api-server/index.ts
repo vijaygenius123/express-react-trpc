9cb027722057c593678006fe43c7678018135334
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2023-01-05 08:22:03 +0530

    Integrate react with trpc server

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -1,7 +1,23 @@
 import express from "express";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import cors from "cors"
+import {router, publicProcedure} from './trpc'
+
+const appRouter = router({
+    greeting: publicProcedure.query(() => 'hello tRPC v10!'),
+})
+
+export type AppRouter = typeof appRouter
 
 const app = express();
+app.use(cors())
 const port = 8080;
+
+// @ts-ignore
+app.use('/trpc', trpcExpress.createExpressMiddleware({
+    router:appRouter,
+
+}))
 
 app.get("/", (req, res) => {
   res.send("Hello from api-server");
